cacad257d66160373d8cd1ea4a81ea4d66c37ede
Sivasankar22 <83053496+Sivasankar22@users.noreply.github.com>
2024-01-09 23:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	// Remove # if present and convert to lowercase
+  shortHex = shortHex.replace(/^#/, '').toLowerCase();
+
+  // If the shortHex has only one character per color, duplicate each character
+  if (shortHex.length === 3) {
+    shortHex = shortHex.replace(/./g, '$&$&');
+  }
+
+  // Add # prefix and return the extended hex code
+  return '#' + shortHex;
 };
 
 // Do not change the code below.
+
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
